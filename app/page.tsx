f5130e5fab660ffc22db08edd6482e3a83f9a26e
Buyaki01 <rittahbuyaki@gmail.com>
2023-05-22 17:48:00 +0300

    Install nextAuth, Add button for login with Google, Remove unused icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      Hello World!
+    <div className="w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="text-white p-2 px-4 rounded-lg">
+          Login with Google
+        </button>
+      </div>
     </div>
   )
 }
